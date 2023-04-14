6871d84d6d769a750396998da3d036cb573dc592
Martin Knaze <mato4899@gmail.com>
2023-04-14 15:52:03 +0200

    add authors routes

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+
+const { urlencoded } = require("express");
+const connectToDB = require("./db/db");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
-app.use(express.static("public"));
-
-const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on("error", (error) => {
-  console.log(error);
-});
-db.once("open", () => {
-  console.log("Connected to mongoose");
-});
-
+// app.use(express.static("public"));
+app.use(urlencoded({ extended: false }));
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
-app.listen(process.env.PORT || 5000);
+const port = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectToDB(process.env.DATABASE_URL).then(() =>
+      console.log("connected to db")
+    );
+    app.listen(port, () => console.log("connected to server on port " + port));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
